12bf1b3693c9c835ca180cf8823cf4e310b865a1
Sheluvboog <111418674+Sheluvboog@users.noreply.github.com>
2023-06-11 19:09:07 +0300

    Додає ДЗ

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom/client'; //бібліотека, яка надає методи для рендерингу
+import { App } from 'components/App/App';
+import './index.css'; // стилізація
 
+// отримуємо корінь додатку + рендер
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+// використовуємо строгий режим
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Діма Берестень
